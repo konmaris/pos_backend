32f87ca6d72aacf07050aa0641f7f9266eb56b80
konmaris <mariskon02@gmail.com>
2024-08-19 00:21:26 +0300

    add

---
+++ test.js
@@ -0,0 +1,7 @@
+let date = new Date();
+let timezoneOffset = date.getTimezoneOffset();
+let pstOffset = -480; // this is the offset for the Pacific Standard Time timezone
+let adjustedTime = new Date(date.getTime() + (pstOffset + timezoneOffset) * 60 * 1000);
+
+console.log(date);
+console.log(timezoneOffset);
