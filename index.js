89046e6deda33fab1f5316f1f45f66f397f0fdcd
konmaris <mariskon02@gmail.com>
2024-08-19 00:24:12 +0300

    add

--- index.js
+++ index.js
@@ -18,6 +18,13 @@ database.on("error", (error) => {
 
 database.once("connected", () => {
   console.log("Database Connected");
+  let date = new Date();
+  let timezoneOffset = date.getTimezoneOffset();
+  let pstOffset = -480; // this is the offset for the Pacific Standard Time timezone
+  let adjustedTime = new Date(date.getTime() + (pstOffset + timezoneOffset) * 60 * 1000);
+
+  console.log(date);
+  console.log(timezoneOffset);
 });
 
 app.get("/", (req, res) => {
