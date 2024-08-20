6df5e26510eff5167bb26af7638d4b7bf908f13e
konmaris <mariskon02@gmail.com>
2024-08-20 22:05:56 +0300

    add

--- src/routes/orders/getCurrentOrders.js
+++ src/routes/orders/getCurrentOrders.js
@@ -5,13 +5,11 @@ const getCurrentOrders = async (req, res) => {
   // find the timestamp of today 12am
   let date = new Date();
 
-  let correctedDate = new Date(date.getTime() - 180 * 60 * 1000);
-
-  correctedDate.setHours(21, 0, 0, 0);
+  date.setHours(0, 0, 0, 0);
 
   //console.log({ correctedDate: correctedDate.getTime() });
 
-  const orders = await Order.find({ orderTime: { $gte: correctedDate.getTime() } }).exec();
+  const orders = await Order.find({ orderTime: { $gte: date.getTime() } }).exec();
 
   res.send(JSON.stringify(orders));
 };
